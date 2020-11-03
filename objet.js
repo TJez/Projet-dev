dcafdcfbb83a5f898bd53c4b562931988fad6a5f
CorentinPeutin <73893307+CorentinPeutin@users.noreply.github.com>
2020-11-03 16:26:10 +0100

    Add files via upload

---
+++ objet.js
@@ -0,0 +1,4 @@
+var mymap = L.map('mapid').setView([47,2], 5);
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(mymap);
