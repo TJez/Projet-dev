05768417f6bccc2d0b509116716028403cc9fe88
CorentinPeutin <73893307+CorentinPeutin@users.noreply.github.com>
2020-11-03 16:57:12 +0100

    Delete objet.js

--- objet.js
+++
@@ -1,4 +0,0 @@
-var mymap = L.map('mapid').setView([47,2], 5);
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(mymap);
